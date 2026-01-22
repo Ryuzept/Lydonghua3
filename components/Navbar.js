5dedd538589ae10f5cbc210df85add8b03e548cf
Ryuzept <dannd5119@gmail.com>
2026-01-23 02:00:00 +0700

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-black text-white px-4 py-3 flex justify-between items-center">
+      <h1 className="text-lg font-bold">Anime Legal</h1>
+      <span className="text-sm text-gray-400">100% Legal</span>
+    </nav>
+  )
+}
