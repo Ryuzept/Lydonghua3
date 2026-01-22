6bfba8992231707cd7e368c0328e0346d4f7e697
Ryuzept <dannd5119@gmail.com>
2026-01-23 02:04:04 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,8 @@
-import Navbar from "../components/Navbar"
-
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-950 text-white">
-      <Navbar />
-
-      <main className="p-4">
-        <h2 className="text-xl font-semibold mb-4">
-          Daftar Anime Legal
-        </h2>
-
-        <p className="text-gray-400">
-          Semua anime berasal dari channel resmi YouTube.
-        </p>
-      </main>
+    <div style={{ padding: 20 }}>
+      <h1>Anime Legal</h1>
+      <p>Website berjalan</p>
     </div>
   )
 }
